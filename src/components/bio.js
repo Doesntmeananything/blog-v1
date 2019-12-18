b7b96b515d17b06db84c7f22235f4f6375b0ea21
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-18 20:03:39 +0300

    Refactor into functional components

--- src/components/bio.js
+++ src/components/bio.js
@@ -12,7 +12,7 @@ import styled from "styled-components";
 
 import { rhythm } from "../utils/typography";
 
-function Bio() {
+const Bio = () => {
   return (
     <StaticQuery
       query={bioQuery}
@@ -46,7 +46,7 @@ function Bio() {
       }}
     />
   );
-}
+};
 
 const bioQuery = graphql`
   query BioQuery {
