5c7075820d40100fc84364fcb2f879f7ce22127d
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-25 00:09:14 +0300

    Add draggable icon wrapper

---
+++ src/components/draggableIcon.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { IconContext } from "react-icons";
+import { useSpring, animated, config, to } from "react-spring";
+import { dist } from "vec-la";
+import { useDrag } from "react-use-gesture";
+
+const DraggableIcon = ({ children }) => {
+  const [props, set] = useSpring(() => ({
+    x: 0,
+    y: 0,
+    scale: 1,
+  }));
+
+  const [{ angle }, setAngle] = useSpring(() => ({
+    angle: 0,
+    config: config.wobbly,
+  }));
+
+  const bind = useDrag(
+    ({ xy, previous, direction, down, movement: [x, y] }) => {
+      set({
+        x: down ? x : 0,
+        y: down ? y : 0,
+        scale: down ? 1.2 : 1,
+        immediate: down,
+      });
+      if (dist(xy, previous) > 2 || !down)
+        setAngle({ angle: Math.atan2(direction[0], -direction[1]) });
+    },
+    { bounds: { left: -200, right: 200, top: -100, bottom: 100 } }
+  );
+
+  return (
+    <animated.div
+      {...bind()}
+      style={{
+        display: "inline-block",
+        cursor: "-webkit-grab",
+        userSelect: "none",
+        transform: to([angle], a => `rotate(${a}rad)`),
+        ...props,
+      }}
+    >
+      <IconContext.Provider value={{ size: "4rem" }}>
+        {children}
+      </IconContext.Provider>
+    </animated.div>
+  );
+};
+
+export default DraggableIcon;
