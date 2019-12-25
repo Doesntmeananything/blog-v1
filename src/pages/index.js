d1214ea1b25d4bdcf966ef7d5a1f2bcbca5fad5b
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-26 00:52:46 +0300

    Change site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,16 @@ import { Link } from "gatsby";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import AniamtedIcons from "../components/animatedIcons";
+import AnimatedHero from "../components/animatedHero";
 import Button from "../components/button";
 
 const IndexPage = ({ location }) => {
-  const siteTitle = "Welcome to my Thoughts, Ideas & Work.";
+  const siteTitle = "Doesn't mean anything";
 
   return (
     <Layout location={location} title={siteTitle}>
-      <AniamtedIcons />
       <SEO title="Home" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
+      <AnimatedHero />
       <Link to="/blog/">
         <Button marginTop="35px">Go to Blog</Button>
       </Link>
